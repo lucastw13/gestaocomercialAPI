230a735c89279acaee0e5ed2922c8b131e28ff07
Lucas Baptista <Lucas.jesus.13>
2022-09-22 17:22:33 -0300

    22-09-2022

--- gestaocomercial.js
+++ gestaocomercial.js
@@ -9,38 +9,64 @@ app.use(
     })
 
 ),
+
     app.use(express.json())
 
 app.post('/ingrediente', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
     var ingrediente = {
         descricao:req.body.descricao,
         tipo:"ingrediente"
     }
     try {
         var ingredienteCriado = await Ingrediente.create(ingrediente);
-        res.status(201).json({status:true,descricao:"ingrediente criado com sucesso",ingrediente:ingredienteCriado})
+        res.status(201).json({status:true,descricao:"ingrediente criado com sucesso!",ingrediente:ingredienteCriado})
     } catch (error) {
         res.status(500).json({ status:false,descricao:error})
     }
 })
 
 app.get('/ingrediente', async(req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
     try {
         const ingrediente = await Ingrediente.find({tipo:"ingrediente"})
-        res.status(201).json({status:true,descricao:"busca realizada com sucesso",ingrediente:ingrediente})
+        res.status(201).json({status:true,descricao:"busca realizada com sucesso!",ingrediente:ingrediente})
     } catch (error) {
         res.status(500).json({ status:false,descricao: error })
     }
 })
 
 app.get('/ingrediente/:codigo', async(req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
     try {
         const ingrediente = await Ingrediente.findById(req.params.codigo)
         if (!ingrediente){
-            res.status(422).json({ status:false,descricao: "ingrediente não encontrado" });
+            res.status(422).json({ status:false,descricao: "ingrediente não encontrado!"});
         
         }
-        res.status(201).json({status:true,descricao:"busca realizada com sucesso",ingrediente:ingrediente})
+        res.status(201).json({status:true,descricao:"busca realizada com sucesso!",ingrediente:ingrediente})
+    } catch (error) {
+        res.status(500).json({ status:false,descricao: error })
+    }
+})
+
+app.delete('/ingrediente/:codigo', async(req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    try {
+        const ingrediente = await Ingrediente.findByIdAndDelete(req.params.codigo)
+        /*if (!ingrediente){
+            res.status(422).json({ status:false,descricao: "ingrediente não encontrado!"});
+        
+        }*/
+        res.status(201).json({status:true,descricao:"ingrediente deletado com sucesso!",ingrediente:ingrediente})
     } catch (error) {
         res.status(500).json({ status:false,descricao: error })
     }
