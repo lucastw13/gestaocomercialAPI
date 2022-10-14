83e59a8d6774f59f0adfae6567bbdca48305c847
Lucas Baptista <Lucas.jesus.13>
2022-10-14 16:40:57 -0300

    feito no final de semana

--- servico/usuario.js
+++ servico/usuario.js
@@ -3,11 +3,17 @@ class usuario {
   static async post(body) {
     var jsonRetorno = { status: 500, json: {} };
     try {
-      const item = await Dado.findOne({nome:body.nome})
-      if (item.senha == body.senha) {
-        jsonRetorno.status = 200
-        jsonRetorno.json = { status: true, descricao: "usuário autenticado com sucesso!",item}
+      const item = await Dado.findOne({ nome: body.nome })
+      if (item != "" && item != undefined) {
+        if (item.senha == body.senha) {
+          jsonRetorno.status = 200
+          jsonRetorno.json = { status: true, descricao: "usuário autenticado com sucesso!", item }
 
+        } else {
+          jsonRetorno.status = 200
+          jsonRetorno.json = { status: false, descricao: "usuário ou senha inválida" }
+
+        }
       } else {
         jsonRetorno.status = 200
         jsonRetorno.json = { status: false, descricao: "usuário ou senha inválida" }
