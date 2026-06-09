e53b212d34459a026d94452dc869f839e51c8537
lucastw13 <lucas_baptista13@hotmail.com>
2026-06-09 18:17:51 -0300

    ajuste limite api

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const Ranking = require('./servico/ranking');
 const DocumentoCompra = require('./servico/documentocompra');
 require('dotenv').config()
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 
 app.use(cors({
     origin: process.env.URL_APP,
